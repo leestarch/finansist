eb11b82cd7bdf65c6835aaf15b09b411dc0651bc
Serengiy <kisaykin20@gmail.com>
2024-11-08 20:06:04 +0700

    Переделал сидеры, вынес логику в сервисы, поправил миграцию, поправил константы

---
+++ app/Services/CategoryService.php
@@ -0,0 +1,128 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\Category;
+use App\Models\Operation;
+use Carbon\Carbon;
+use Illuminate\Support\Collection;
+
+final class CategoryService
+{
+    public static function getCategoryTree(
+        bool $withSum = false, ?string $startDate = null, ?string $endDate = null, string $groupBy = 'daily'
+    ): array
+    {
+        if (!$startDate) {
+            $startDate = now()->startOfMonth()->toDateString();
+        }
+        if (!$endDate) {
+            $endDate = now()->endOfMonth()->toDateString();
+        }
+
+        $query = Category::query();
+
+        if ($withSum) {
+            $query->with([
+                'operations'  => function($q) use ($startDate, $endDate) {
+                    $q->whereBetween('date_at', [$startDate, $endDate]);
+                },
+                'children.operations'  => function($q) use ($startDate, $endDate) {
+                    $q->whereBetween('date_at', [$startDate, $endDate]);
+                },
+                'children.children.operations'  => function($q) use ($startDate, $endDate) {
+                    $q->whereBetween('date_at', [$startDate, $endDate]);
+                },
+                'children.children.children.operations' => function($q) use ($startDate, $endDate) {
+                    $q->whereBetween('date_at', [$startDate, $endDate]);
+                },
+                'children.children.children.children.operations' => function($q) use ($startDate, $endDate) {
+                    $q->whereBetween('date_at', [$startDate, $endDate]);
+                },
+                'children.children.children.children.children.operations' => function($q) use ($startDate, $endDate) {
+                    $q->whereBetween('date_at', [$startDate, $endDate]);
+                },
+                'children.children.children.children.children.children.operations' => function($q) use ($startDate, $endDate) {
+                    $q->whereBetween('date_at', [$startDate, $endDate]);
+                },
+            ]);
+        }
+
+
+        $categories = $query->get();
+
+        $result = [];
+
+        foreach ($categories->whereNull('parent_id') as $rootCategory) {
+            $rootCategory->totals = self::calculateDailyTotals(
+                $rootCategory->operations, $rootCategory->children, $groupBy, true
+            );
+            $rootCategory->children = self::buildTree($rootCategory->children, $groupBy, $rootCategory->totals);
+            $result[] = $rootCategory;
+        }
+
+        return $result;
+    }
+
+    private static function calculateDailyTotals(
+        $operations, $children = null, string $groupBy = 'daily', bool $isRoot = false, array $rootTotals = []
+    ): array
+    {
+        $totals = [];
+        $dateFormat = match ($groupBy) {
+            'daily' => 'd-m-Y',
+            'weekly' => 'W-o',
+            'monthly' => 'm-Y',
+            'quarterly' => 'Q-Y',
+        };
+
+        // Aggregate totals for this category's operations
+        foreach ($operations as $operation) {
+            $date = Carbon::parse($operation->date_at);
+            $dateKey = $groupBy === 'quarterly' ? $date->quarter . '-' . $date->format('Y') : $date->format($dateFormat);
+
+            if (!isset($totals[$dateKey])) {
+                $totals[$dateKey] = ['sum' => 0, 'percentage_of_root' => 0];
+            }
+
+            $operation->sber_direction === Operation::CREDIT
+                ? $totals[$dateKey]['sum'] += $operation->sber_amountRub
+                : $totals[$dateKey]['sum'] -= $operation->sber_amountRub;
+
+        }
+
+        if ($children instanceof Collection) {
+            foreach ($children as $child) {
+                $childTotals = self::calculateDailyTotals($child->operations, $child->children, $groupBy, false, $rootTotals);
+                foreach ($childTotals as $date => $data) {
+                    if (!isset($totals[$date])) {
+                        $totals[$date] = ['sum' => 0, 'percentage_of_root' => 0];
+                    }
+                    $totals[$date]['sum'] += $data['sum'];
+                }
+            }
+        }
+
+        if (!$isRoot && !empty($rootTotals)) {
+            foreach ($totals as $date => &$data) {
+                $rootSumForDate = $rootTotals[$date]['sum'] ?? 0;
+                $data['percentage_of_root'] = number_format($rootSumForDate > 0 ? ($data['sum'] / $rootSumForDate) * 100 : 0, 2);
+            }
+        }
+
+        return $totals;
+    }
+
+    private static function buildTree(Collection $categories, string $groupBy, array $rootTotals = []): array
+    {
+        foreach ($categories as $category) {
+            $category->totals = self::calculateDailyTotals($category->operations, $category->children, $groupBy, false, $rootTotals);
+
+            if ($category->children->isNotEmpty()) {
+                $childTree = self::buildTree($category->children, $groupBy, $rootTotals);
+                $category->children = collect($childTree);
+            }
+        }
+        return $categories->toArray();
+    }
+}
