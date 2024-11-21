5d70b0665b59bd3d13f5ef3106ef79fef9731eb0
Serengiy <kisaykin20@gmail.com>
2024-11-21 15:57:34 +0700

    Команда для рестора операции-категории

---
+++ app/Console/Commands/Dev/OperationCategoriesTableCommand.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Console\Commands\Dev;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\DB;
+
+class OperationCategoriesTableCommand extends Command
+{
+    protected $signature = 'operation.categories.table {--restore} {--validate}';
+    public function handle()
+    {
+
+        DB::table('categories_operations')->truncate();
+
+        if($this->option('restore')){
+            $sqlFilePath = storage_path('app/private/dodo_sber_transaction_budget_category.sql');
+            $sqlContent = file_get_contents($sqlFilePath);
+            DB::unprepared($sqlContent);
+        }
+        if($this->option('validate')) {
+            Artisan::call('operation.validate');
+        }
+    }
+}
