f63ef09f89c693fc0051b281ce24a29361a80e98
Serengiy <kisaykin20@gmail.com>
2024-11-29 19:12:05 +0700

    seeder

---
+++ app/Console/Commands/Dev/SeedOperationCommand.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Console\Commands\Dev;
+
+use App\Models\Operation;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Http;
+
+class SeedOperationCommand extends Command
+{
+    protected $signature = 'seed.operation';
+    public function handle()
+    {
+        if (app()->environment() === 'production') {
+            Operation::query()->truncate();
+            return;
+        }
+
+        $operations = Operation::query();
+        $operations->chunk(1000, function ($chunk) {
+            $data = $chunk->toArray();
+            $response = Http::withoutVerifying()->accept('application/json')
+                ->post('https://fin.lookin.team/api/seed', [
+                     'data' => $data
+                ]);
+
+            if ($response->failed()) {
+                dump($response->json());
+            }
+        });
+    }
+}
